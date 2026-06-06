6bd088e1a4b236120241a9f0354a1cc518d51c0b
Panchadarla Veditha Keerti Rani <veditha888@gmail.com>
2026-06-07 03:44:53 +0530

    Enhance Node.js usage documentation
    Added additional instructions for using Node.js and npm, including package installation, dependencies, and scripts.

--- node.js
+++ node.js
@@ -19,13 +19,26 @@ require: func to use modules(common js)
 npm init -y or just npm init (keep pressing enter after it to set default values): used to create package.json
 package.json: basically keeps the account of the whole project
 
-to run a node code use //node filename// 
+to run a node code use //node ./filename// 
 
+to install any package from NPM: npm i packagename   if any particular version of it then npm i packagename@version 
+whenever u install smthng it becomes a dependency.
+to uninstall any package: npm uninstall packagename
+
+//dependencies and devdependencies:
+dependency: contains the packages and the packages other packages are dependet upon.
+devdependency: the packages that are only useful in development. but when the app is deployed, we wouldnt be using these packages
+
+//scripts:
+it basically contains the commands that we can run for that app. like its the known set of commands which in front there is no need to
+add run like u can just run them by using npm test or anything. usualyy contains start and test. if you want to run any command whihc
+is not in the scripts then u shd add it to scripts and then run like npm run dev or smthng.
+
+  
 //GLOBAL SHARING USING MODULE AND REQUIRE
 modules.export = {bla bla} can be used to declare the variables that we want to share globally.. like to all the files
 in order to access them from other files, we need to use-   require{./file_name_where_we_want_to_accessthemodule}
-
-
+                                                                    
 //file system operations read from official node js doc
 
 //http module: reqd for sending or receiving anything
